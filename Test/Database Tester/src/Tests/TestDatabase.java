4efd35c2a9955a3bed75c6a0b3b2f7766aba8c08
Roland Abrahantes <rabrahantes@ggc.edu>
2016-04-04 12:44:53 +1200

    Instructions

---
+++ Test/Database Tester/src/Tests/TestDatabase.java
@@ -0,0 +1,30 @@
+/*=============================================================================
+ |   Assignment:  My Beauty Application
+ |       Author:  Roland Abrahantes
+ |       Grader:  Dr. Gunay ITEC 3860
+ |
+ |       Course:  ITEC 3860
+ |   Instructor:  Dr. Gunay
+ |     Due Date:  3/14/2016
+ |
+ |  Description:  This class provides the user with how to use instructions for the Program.
+ |
+ |     Language:  JAVA and SQL
+ | Ex. Packages:  JAVAFX
+ |                                
+ *===========================================================================*/
+
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDatabase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
